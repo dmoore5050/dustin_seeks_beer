2758cccb045ca29e1863957a3313af8dcbb3ceef
dmoore5050 <dustinmoore@gmail.com>
2018-03-02 12:37:38 -0600

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  devise_for :admins, controllers: {
+    registrations: "admins/registrations",
+    confirmations: "admins/confirmations",
+    passwords:     "admins/passwords",
+    sessions:      "admins/sessions",
+    unlocks:       "admins/unlocks"
+  }
 
   resources :beers do
     member do
