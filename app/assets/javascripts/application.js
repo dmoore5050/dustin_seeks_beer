f05a5011fc6be68958597c974006b249c28f4a1c
dmoore5050 <dustinmoore@gmail.com>
2018-03-02 16:07:46 -0600

    more fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,20 @@
 //= require_tree .
 
 function flashAlert(message, klass, delay) {
+
   rand = Math.random().toString(36).substring(7);
+
   if ($('.flashAlertAlpha').length <= 0) {
-    $("body").append('<div id="success-flash-' + rand + '" class="col-lg-6 col-xs-8 alert flashAlertAlpha alert-' + klass + ' center" style="position: fixed; top: 50px; z-index: 9999; left: 50%; transform: translateX(-50%);"><button class="close" data-dismiss="alert" name="button" type="button">x</button>' + message + '</div>')
+    $("body").append('<div id="success-flash-' + rand + '" class="alert flashAlertAlpha alert-' + klass + ' center" style="position: fixed; top: 30px; z-index: 9999; left: 50%; transform: translateX(-50%); width: 90%; font-size: 14px; line-height: 22px; text-align: center;"><button class="alert-close close" name="button" type="button">x</button>' + message + '</div>')
     $("#success-flash-" + rand).delay(delay).fadeOut(function(){
       $("#success-flash-" + rand).remove();
     });
   } else {
     $(".flashAlertAlpha").removeClass('alert-success alert-info alert-warning alert-danger').addClass('alert-' + klass);
-    $(".flashAlertAlpha").empty().html('<button class="close" data-dismiss="alert" name="button" type="button">x</button>' + message).show();
+    $(".flashAlertAlpha").empty().html('<button class="alert-close close" name="button" type="button">x</button>' + message).show();
   }
+
+  $('body').on('click', '.alert-close', function(e) {
+    $('.flashAlertAlpha').remove();
+  });
 }
